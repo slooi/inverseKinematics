9fd53d8ad3e245bed0c495e305a381fd5cae0437
slooi <s.looi2@outlook.com>
2020-04-08 03:17:26 +1200

    Bug: buffer overflow

---
+++ main.js
@@ -0,0 +1,125 @@
+console.log('loaded main.js')
+
+let points = []			// x,y 3*2
+let arms = [100,50,50,100]	// points.length/2 - 1
+let angles = [1,0,50,100]	// points.length/2 - 1
+let start = [0,0]
+let end = [80,30]
+
+
+for(let i=0;i<100;i++){
+	arms[i] = 2
+	angles[i] = 2
+}
+
+//
+let oldTime = new Date()
+const fps = 20
+
+
+// check if in range
+
+// populate points
+popPoints()
+
+line(start[0],start[1],start[0]+5,start[1]+5,0,1,0)
+line(end[0],end[1],end[0]+5,end[1]+5,1,0,0)
+render()
+console.log(points)
+looper()
+
+
+
+
+function eachFrame(){
+	clear()
+	backwardsPass()
+	forwardsPass()
+	render()
+}
+
+
+function looper(){
+
+	if(new Date()-oldTime > 1000/fps){
+		eachFrame()
+		oldTime = new Date()
+	}
+
+	requestAnimationFrame(looper)
+}
+
+//
+function popPoints(){
+	nextPoint = start
+	points.push(...start)
+	for(let i=0;i<arms.length;i++){
+		newPoint = findNextPoint(nextPoint[0],nextPoint[1],arms[i],angles[i])
+		// line(newPoint[0],newPoint[1],newPoint[0]+5,newPoint[1]+5,0,0,1)
+		// line(...nextPoint,...newPoint)
+	
+		nextPoint=newPoint
+		points.push(...nextPoint)
+	}
+}
+
+
+// forwards pass
+function backwardsPass(){
+	points[points.length-1] = end[1]	// y
+	points[points.length-2] = end[0]	// x
+	// line( end[0],end[1],end[0]+50,end[1]+50,1,0.5,1)
+	// console.log(points)
+	for(let i=points.length-4;i>=0;i-=2){
+		newPoint = backPoint(points[i+2],points[i+3],points[i],points[i+1],arms[i/2])
+		
+		// line( newPoint[0],newPoint[1],newPoint[0]+50,newPoint[1]+50,1,0.5,1)
+		points[i]	= newPoint[0]
+		points[i+1]	= newPoint[1]
+		// console.log(newPoint)
+	}
+}
+
+
+function forwardsPass(){
+	points[0] = start[0]
+	points[1] = start[1]
+	for(let i=0;i<points.length-2;i+=2){
+		newPoint = backPoint(points[i],points[i+1],points[i+2],points[i+3],arms[i/2])
+		// line( newPoint[0],newPoint[1],newPoint[0]+50,newPoint[1]+50,1,1,1)
+		line(points[i],points[i+1],newPoint[0],newPoint[1],0,1,1)
+		
+	line( end[0],end[1],end[0]+50,end[1]+50,1,1,1)
+		points[i+2]	= newPoint[0]
+		points[i+3]	= newPoint[1]
+	}
+}
+
+render()
+
+
+// FUNCTIONS
+// 
+// function find previous point
+function backPoint(currentX,currentY,targetX,targetY,len){
+	// cal angle
+	const dir = Math.atan2(targetY-currentY,targetX-currentX)
+	return findNextPoint(currentX,currentY,len,dir)
+}
+
+//  
+function findNextPoint(x,y,len,dir){
+	// input:
+	//	x,y,lenght,angle
+	// output:
+	//  x,y
+	// dir = dir/180*Math.PI
+	return [x+len*Math.cos(dir),y+len*Math.sin(dir)]
+}
+
+
+
+canvas.addEventListener('mousemove',e=>{
+	end[0]=e.offsetX-canvas.width/2
+	end[1]=-e.offsetY+canvas.width/2
+})
---
+++ webgl.js
@@ -0,0 +1,165 @@
+console.log('webgl.js loaded')
+
+// config vars
+const lineRGB = [1,1,1]
+
+const comps_per_ele = 5;
+
+let offsetCounter = 0
+const BYTES_PER_LINE = 10*4
+const MAX_NUM_OF_LINES = 10000
+
+// shader source
+const vsSource = document.getElementById('vsSource').innerText
+const fsSource = document.getElementById('fsSource').innerText
+
+// canvas
+const canvas = document.createElement('canvas')
+canvas.width = 300
+canvas.height = 300
+document.body.append(canvas)
+
+let gl = canvas.getContext('webgl',{antialias:false})
+if(!gl){
+	gl = canvas.getContext('experimental-webgl')
+}
+if(!gl){
+	alert('ERROR: webgl not supported.')
+}
+
+gl.viewport(0,0,canvas.width,canvas.height)
+gl.clearColor(0.5,0.4,0.7,1)
+gl.clear(gl.COLOR_BUFFER_BIT)
+
+// program
+const program = buildProgram()
+gl.useProgram(program)
+
+// location
+// attrib
+const attribLocations = []
+for(let i=0;i<gl.getProgramParameter(program,gl.ACTIVE_ATTRIBUTES);i++){
+	attribName = gl.getActiveAttrib(program,i).name
+	attribLocations[attribName] = gl.getAttribLocation(program,attribName)
+}
+
+const uniformLocations = []
+for(let i=0;i<gl.getProgramParameter(program,gl.ACTIVE_UNIFORMS);i++){
+	uniformName = gl.getActiveUniform(program,i).name
+	uniformLocations[uniformName] = gl.getUniformLocation(program,uniformName)
+}
+
+// data
+let data = [
+	0,0,			1,1,1,
+	-0.7,-0.3,1,1,1,
+	0.8,-0.1,	1,1,1,
+	0,0.8,		1,1,1,
+]
+let data2 = [
+	0,0, 1,1,1,
+]
+
+// buffer
+const dataBuffer = gl.createBuffer()
+gl.bindBuffer(gl.ARRAY_BUFFER,dataBuffer)
+gl.bufferData(gl.ARRAY_BUFFER,BYTES_PER_LINE*MAX_NUM_OF_LINES,gl.DYNAMIC_DRAW)
+
+// pointer
+gl.vertexAttribPointer(
+	attribLocations.a_Position,
+	2,
+	gl.FLOAT,
+	0,
+	5*Float32Array.BYTES_PER_ELEMENT,
+	0
+)
+gl.enableVertexAttribArray(attribLocations.a_Position)
+gl.vertexAttribPointer(
+	attribLocations.a_Color,
+	3,
+	gl.FLOAT,
+	0,
+	5*Float32Array.BYTES_PER_ELEMENT,
+	2*Float32Array.BYTES_PER_ELEMENT
+)
+gl.enableVertexAttribArray(attribLocations.a_Color)
+
+// texture
+
+// unifom
+gl.uniform1f(uniformLocations.u_RadInv,2/canvas.width)
+
+// render
+// gl.drawArrays(gl.POINTS,0,data.length/comps_per_ele)
+
+// gl.drawArrays(gl.LINES,0,data.length/comps_per_ele)
+// line(0,0,0,0.8,0.2)
+// line(20,0,0,0.8,-0.2)
+// line(-50,-50,80,-80)
+// line(50,-50,80,-80)
+// // line(40,-0.5,-0.5,0.8,-0.8)
+// render()
+
+// FUNCTIONS
+// function dot(x,y){
+// 	const data = new Float32Array([
+// 		x1,y1,lineRGB[0],lineRGB[1],lineRGB[2],
+// 		x2,y2,lineRGB[0],lineRGB[1],lineRGB[2],
+// 	])
+// 	console.log(data)
+// gl.Bu(gl.ARRAY_BUFFER,)	offsetCounter += BYTES_PER_LINE
+// }
+// function renderPoints(){
+// 	gl.drawArrays(gl.POINTS,0,data2.length/2)
+// }
+
+function resetCounter(){
+	offsetCounter = 0
+}
+
+function line(x1,y1,x2,y2,r=1,g=1,b=1){
+	const data = new Float32Array([
+		x1,y1,r,g,b,
+		x2,y2,r,g,b,
+	])
+	// console.log(data)
+	gl.bufferSubData(gl.ARRAY_BUFFER,offsetCounter,data)
+	offsetCounter += BYTES_PER_LINE
+}
+
+function render(){
+	gl.drawArrays(gl.LINES,0,offsetCounter/BYTES_PER_LINE*2)
+}
+function clear(){
+	gl.clear(gl.COLOR_BUFFER_BIT)
+	gl.bindBuffer(gl.ARRAY_BUFFER,dataBuffer)
+	gl.bufferData(gl.ARRAY_BUFFER,BYTES_PER_LINE*MAX_NUM_OF_LINES,gl.DYNAMIC_DRAW)
+}
+
+
+function buildShader(type,source){
+	const shader = gl.createShader(type)
+	gl.shaderSource(shader,source)
+	gl.compileShader(shader)
+	if(!gl.getShaderParameter(shader,gl.COMPILE_STATUS)){
+		throw new Error('ERROR: compiling shader of type '+type+' . Info: '+gl.getShaderInfoLog(shader))
+	}
+	return shader
+}
+
+function buildProgram(){
+	const program = gl.createProgram()
+	gl.attachShader(program,buildShader(gl.VERTEX_SHADER,vsSource))
+	gl.attachShader(program,buildShader(gl.FRAGMENT_SHADER,fsSource))
+	gl.linkProgram(program)
+	gl.validateProgram(program)
+
+	if(!gl.getProgramParameter(program,gl.LINK_STATUS)){
+		throw new Error('ERROR: linking program. Info: '+gl.getProgramInfoLog(program))
+	}
+	if(!gl.getProgramParameter(program,gl.VALIDATE_STATUS)){
+		throw new Error('ERROR: validating program. Info: '+gl.getProgramInfoLog(program))
+	}
+	return program
+}
